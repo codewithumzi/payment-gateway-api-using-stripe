ef676c670c6fd76604c371eb28ce80100b0585d8
Umar Ashraf Lone <theistummer23@gmail.com>
2024-01-13 05:43:44 +0530

    11-01-2024 payment gateway api using stripe

---
+++ src/main/java/com/payments/controller/PaymentController.java
@@ -0,0 +1,37 @@
+package com.payments.controller;
+
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import com.stripe.param.PaymentIntentCreateParams;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/payment")
+public class PaymentController {
+    @Value("${stripe.api.key}")
+    private String stripeApiKey;
+
+    //http://localhost:8080/api/payment/create-payment-intent?amount=1000
+    @PostMapping("/create-payment-intent")
+    public String createPaymentIntent(@RequestParam Integer amount) {
+        Stripe.apiKey = stripeApiKey;
+        try {
+            PaymentIntent intent = PaymentIntent.create(
+                    new PaymentIntentCreateParams.Builder()
+                            .setCurrency("usd")
+                            .setAmount((long) amount*100)
+                            .build()
+            );
+            return generateResponse(intent.getClientSecret());
+        } catch (StripeException e) {
+            return generateResponse("Error creating PaymentIntent :" + e.getMessage());
+        }
+    }
+    private String  generateResponse(String clientSecret) { return "{\"clientSecret\":\""+ clientSecret +"\"}";}
+
+}
